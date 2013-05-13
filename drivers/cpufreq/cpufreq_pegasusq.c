5358e1debae2874c0d76707ab046eb4642729196
Gustavo <gustavo.santos0106@gmail.com>
2013-05-13 17:03:11 -0300

    Cpufreq: Add boostpulse interface to pegasusq (now working as it should)

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -151,6 +151,8 @@ static unsigned int get_nr_run_avg(void)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(5)
 #define DEF_FREQUENCY_UP_THRESHOLD		(90)
+#define DEFAULT_FREQ_BOOST_TIME      (500000)
+u64 freq_boosted_time;
 
 /* for multiple freq_step */
 #define DEF_UP_THRESHOLD_DIFF	(5)
@@ -176,7 +178,7 @@ static unsigned int get_nr_run_avg(void)
 #define DEF_START_DELAY				(0)
 
 #define UP_THRESHOLD_AT_MIN_FREQ		(85)
-#define FREQ_FOR_RESPONSIVENESS			(600000)
+#define FREQ_FOR_RESPONSIVENESS			(500000)
 /* for fast decrease */
 #define FREQ_FOR_FAST_DOWN				(800000)
 #define UP_THRESHOLD_AT_FAST_DOWN		(95)
@@ -201,7 +203,7 @@ static int hotplug_rq[4][2] = {
 };
 
 static int hotplug_freq[4][2] = {
-	{0, 600000},
+	{0, 800000},
 	{500000, 800000},
 	{500000, 800000},
 	{500000, 0}
@@ -264,6 +266,9 @@ static struct dbs_tuners {
 	unsigned int ignore_nice;
 	unsigned int sampling_down_factor;
 	unsigned int io_is_busy;
+        unsigned int boosted;
+        unsigned int freq_boost_time;
+        unsigned int boostfreq;
 	/* pegasusq tuners */
 	unsigned int freq_step;
 	unsigned int cpu_up_rate;
@@ -287,6 +292,8 @@ static struct dbs_tuners {
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.down_differential = DEF_FREQUENCY_DOWN_DIFFERENTIAL,
 	.ignore_nice = 0,
+        .freq_boost_time = DEFAULT_FREQ_BOOST_TIME,
+        .boostfreq = 800000,
 	.freq_step = DEF_FREQ_STEP,
 	.cpu_up_rate = DEF_CPU_UP_RATE,
 	.cpu_down_rate = DEF_CPU_DOWN_RATE,
@@ -488,6 +495,9 @@ show_one(io_is_busy, io_is_busy);
 show_one(up_threshold, up_threshold);
 show_one(sampling_down_factor, sampling_down_factor);
 show_one(ignore_nice_load, ignore_nice);
+show_one(boostpulse, boosted);
+show_one(boosttime, freq_boost_time);
+show_one(boostfreq, boostfreq);
 show_one(down_differential, down_differential);
 show_one(freq_step, freq_step);
 show_one(cpu_up_rate, cpu_up_rate);
@@ -505,6 +515,49 @@ static ssize_t show_hotplug_lock(struct kobject *kobj,
 {
 	return sprintf(buf, "%d\n", atomic_read(&g_hotplug_lock));
 }
+
+static ssize_t store_boosttime(struct kobject *kobj, struct attribute *attr,
+        const char *buf, size_t count)
+{
+  unsigned int input;
+  int ret;
+
+  ret = sscanf(buf, "%u", &input);
+  if (ret != 1)
+    return -EINVAL;
+
+  dbs_tuners_ins.freq_boost_time = input;
+  return count;
+}
+
+
+static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
+        const char *buf, size_t count)
+{
+  int ret;
+  unsigned long val;
+
+  ret = kstrtoul(buf, 0, &val);
+  if (ret < 0)
+    return ret;
+
+  dbs_tuners_ins.boosted = 1;
+  freq_boosted_time = ktime_to_us(ktime_get());
+  return count;
+}
+
+static ssize_t store_boostfreq(struct kobject *a, struct attribute *b,
+           const char *buf, size_t count)
+{
+  unsigned int input;
+  int ret;
+  ret = sscanf(buf, "%u", &input);
+  if (ret != 1)
+    return -EINVAL;
+  dbs_tuners_ins.boostfreq = input;
+  return count;
+}
+
 
 #define show_hotplug_param(file_name, num_core, up_down)		\
 static ssize_t show_##file_name##_##num_core##_##up_down		\
@@ -863,6 +916,9 @@ define_one_global_rw(io_is_busy);
 define_one_global_rw(up_threshold);
 define_one_global_rw(sampling_down_factor);
 define_one_global_rw(ignore_nice_load);
+define_one_global_rw(boostpulse);
+define_one_global_rw(boosttime);
+define_one_global_rw(boostfreq);
 define_one_global_rw(down_differential);
 define_one_global_rw(freq_step);
 define_one_global_rw(cpu_up_rate);
@@ -884,6 +940,9 @@ static struct attribute *dbs_attributes[] = {
 	&sampling_down_factor.attr,
 	&ignore_nice_load.attr,
 	&io_is_busy.attr,
+        &boostpulse.attr,
+        &boosttime.attr,
+        &boostfreq.attr,
 	&down_differential.attr,
 	&freq_step.attr,
 	&cpu_up_rate.attr,
@@ -1155,6 +1214,14 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	int rq_avg = 0;
 	policy = this_dbs_info->cur_policy;
 
+/* Only core0 controls the boost */
+  if (dbs_tuners_ins.boosted && policy->cpu == 0) {
+    if (ktime_to_us(ktime_get()) - freq_boosted_time >=
+          dbs_tuners_ins.freq_boost_time) {
+      dbs_tuners_ins.boosted = 0;
+    }
+  }
+
 	hotplug_history->usage[num_hist].freq = policy->cur;
 	hotplug_history->usage[num_hist].rq_avg = get_nr_run_avg();
 
@@ -1250,6 +1317,13 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	if (hotplug_history->num_hist  == max_hotplug_rate)
 		hotplug_history->num_hist = 0;
 
+/* check for frequency boost */
+  if (dbs_tuners_ins.boosted && policy->cur < dbs_tuners_ins.boostfreq) {
+    dbs_freq_increase(policy, dbs_tuners_ins.boostfreq);
+    dbs_tuners_ins.boostfreq = policy->cur;
+    return;
+  }
+
 	/* Check for frequency increase */
 	if (policy->cur < dbs_tuners_ins.freq_for_responsiveness)
 		up_threshold = dbs_tuners_ins.up_threshold_at_min_freq;
@@ -1304,6 +1378,11 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		freq_next = max_load_freq /
 			(dbs_tuners_ins.up_threshold -
 			 dbs_tuners_ins.down_differential);
+
+if (dbs_tuners_ins.boosted &&
+        freq_next < dbs_tuners_ins.boostfreq) {
+      freq_next = dbs_tuners_ins.boostfreq;
+    }
 
 		/* No longer fully busy, reset rate_mult */
 		this_dbs_info->rate_mult = 1;
